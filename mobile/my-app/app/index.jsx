e673824d75ba6b2ad628dc197b617f065e98061e
ANDRIELLI DOS SANTOS HENCKER <andrielli_hencker@estudante.sesisenai.org.br>
2024-11-12 16:12:56 -0300

    andrichata

--- mobile/my-app/app/index.jsx
+++ mobile/my-app/app/index.jsx
@@ -1,122 +1,144 @@
 import { useState } from "react";
-import { View, Text, TextInput, Pressable, StyleSheet, SafeAreaView } from "react-native";
+import { View, Text, TextInput, Pressable, StyleSheet, SafeAreaView, Image,} from "react-native";
+import {Link} from 'expo-router';
 
 const style = StyleSheet.create({
-    input: {
-        width: '100%',
-        margin: 5,
-        backgroundColor: '#799fbf',
-        borderRadius: '15px',
-        alignItems: 'center',
-        justifyContent: 'center',
-        textAlign: 'center',
-        padding: 5
-        
-    },
     container: {
+        flex: 1,
+        backgroundColor: '#1b6a87',
         alignItems: 'center',
-        justifyContent:'center',
-        backgroundColor:'#799fbf',
-        flex: 1
-    },
-
-    button:{
         justifyContent: 'center',
-        margin:15,
-        alignItems:'center',
-        color:'grey',
-        paddingHorizontal: 12,
-        paddingVertical: 6,
-        backgroundColor: 'black',
-        borderRadius: 15
+        paddingHorizontal: 20,
     },
-    titulo:{
-        fontSize: 49,
-        fontFamily: 'Times New Roman',
-        fontWeight:'760',
-
+    card: {
+        backgroundColor: '#868480',
+        padding: 25,
+        borderRadius: 15,
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 10,
+        elevation: 5,
+        width: '100%',
+        maxWidth: 400,
+        alignItems: 'center',
     },
-    box:{
-        shadowOpacity: 0.30,  
-        shadowRadius: 9, 
-        padding: 50,
-        backgroundColor:'white',
-        justifyContent: 'center',
+    titulo: {
+        fontSize: 28,
+        fontWeight: '600',
+        color: '#333',
+        marginBottom: 20,
+    },
     
-       },
+    input: {
+        height: 50,
+        width: '100%',
+        borderColor: '#171716',
+        borderWidth: 1,
+        borderRadius: 10,
+        paddingHorizontal: 15,
+        fontSize: 16,
+        color: '#333',
+        marginBottom: 15,
+        backgroundColor: '#d6d6d4',
+    },
+    botao: {
+        backgroundColor: '#1b6a87',
+        borderRadius: 10,
+        width: '100%',
+        paddingVertical: 15,
+        alignItems: 'center',
+        marginTop: 15,
+    },
+    botaotexto: {
+        color: 'e4e2dd',
+        fontSize: 16,
+        fontWeight: '600',
+    },
+    img:{
+        width: 175,
+        height: 210,
+      borderRadius: 50,
+      marginBottom: 20,
+      alignSelf: 'center', 
+      width: 270, 
+      height: 270,
+      borderRadius: 12,
+      marginRight: 8,
+    },
+    ViewLink: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        margin: 20,
+    },
+    ViewTexto: {
+        fontSize: 16,
+        marginRight: 8,
+    },
+    TextoLink: {
+        fontSize: 16,
+        color: 'white',
+    },
 
-       botaotexto:{
-        color:'white',
-        alignItems:'center',
-        justifyContent:'center'
-       }
+});
 
-})
-
-export default SinUp = () => {
-    //const email = ''
-
-    const [email, setEmail] = useState('')
-    const [nome, setNome] = useState('')
-    const [senha, setSenha] = useState('')
+export default function SignUp() {
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
 
     const registrarUsuario = async function () {
-        if (!nome || !email || !senha) {
-            console.log('os parametros nome, email e senha devem ser fornecidos')
-            return
+        if (!email || !senha) {
+            console.log('os parametros email e senha devem ser fornecidos');
+            return;
         }
-        const resposta = await fetch('https://taskhub-s37f.onrender.com/auth/signup',{
+        const resposta = await fetch('https://taskhub-s37f.onrender.com/auth/signup', {
             method: 'POST',
             headers: {
-            Accept: 'application/json',
-            'Content-type': 'application/json',
-        },
-            body: JSON.stringify({ name: nome, email: email, password: senha })
-        })
-        
-    if (!resposta) {
-        console.log('erro')
-    } else if (resposta.status == 200) {
-        console.log('user criado com sucesso')
-    } else {
-        console.log('ocorreu um erro')
-    }
-}
+                Accept: 'application/json',
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify({ email, senha }),
+        });
 
-return (
-    <SafeAreaView style={style.container}>
-        <View style={style.box}>
-        <View >
-            <Text style={style.titulo}>Registre-se</Text>
-        </View>
-        <View style={style.inputView}>
-            <TextInput
-                style={style.input}
-                onChangeText={(text) => setEmail(text)}
-                value={email}
-                placeholder="Digite seu email zika"
-            />
-            <TextInput
-                style={style.input}
-                onChangeText={(text) => setNome(text)}
-                value={nome}
-                placeholder="Seu nome animal"
-            /><TextInput
-                style={style.input}
-                onChangeText={(text) => setSenha(text)}
-                value={senha}
-                placeholder="Sua senha segura"
-                secureTextEntry={true}
-            />
+        if (!resposta) {
+            console.log('erro');
+        } else if (resposta.status === 200) {
+            console.log('Login foi bem sucdido');
+        } else {
+            console.log('ocorreu um erro');
+        }
+    };
 
-            <View style={style.button} >
-                <Pressable onPress={registrarUsuario}>
-                    <Text style={style.botaotexto}>Cadastrar</Text>
+    return (
+        <SafeAreaView style={style.container}>
+            <View style={style.card}>
+                
+                <Image style={style.img} source={require('../app/img/fone.png')}/>
+
+                <TextInput
+                    style={style.input}
+                    onChangeText={setEmail}
+                    value={email}
+                    placeholder="Email:"
+                />
+                <TextInput
+                    style={style.input}
+                    onChangeText={setSenha}
+                    value={senha}
+                    placeholder="Senha:"
+                    secureTextEntry={true}
+                />
+
+                <Pressable style={style.botao} onPress={registrarUsuario}>
+                    <Text style={style.botaotexto}>Entrar</Text>
                 </Pressable>
-            </View>
-</View>
-        </View>
-    </SafeAreaView>
+                <View style={style.ViewLink}>
+                    <Text style={style.ViewTexto}>Não tem uma conta?</Text>
+                    <Link href='/inicio'>
+                        <Text style={style.TextoLink}>Crie uma</Text>
+                    </Link>
+                </View>
 
-)
+            </View>
+        </SafeAreaView>
+    );
 }
